21e0c3880b5b2f1c1f0cd95df6b14d0ae0433d36
natherFlat <38530068+natherFlat@users.noreply.github.com>
2018-06-25 01:15:54 +0300

    Delete diplom.py

--- diplom.py
+++
@@ -1,7 +0,0 @@
-import urllib2
-f = urllib2.urlopen('http://vikingi-online.com/online/4-season/')
-out = open('site.txt','w')
-s = f.read()
-text = s[s.find('<body'):s.find('</body>') ]
-s = ''
-out.write(text)
